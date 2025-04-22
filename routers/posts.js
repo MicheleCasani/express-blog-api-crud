cc1fed8d651f69943b94958ee370558a25742484
MicheleCasani <casanimichele@gmail.com>
2025-04-22 15:07:45 +0200

    [UPD] add other funtion on posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -13,44 +13,22 @@ const postsController = require('../controllers/postsController.js');
 // DEFINISCO LE ROTTE PER I POST
 
 // index
-router.get('/', (req, res) => {
-    const tags = req.query.tags;
-    let filteredTags = posts;
-    if (tags) {
-        filteredTags = posts.filter(function (element) {
-            return element.tags.includes(tags);
-        })
-    }
-    res.json(filteredTags)
-});
+router.get('/', postsController.index);
 
 // show
-router.get('/:id', (req, res) => {
-    const post = posts.find(function (element) {
-        return element.id == req.params.id
-    });
-    res.json(post)
-});
+router.get('/:id', postsController.index);
 
 // store
-router.post('/', (req, res) => {
-    res.json('creo un nuovo elemento')
-});
+router.get('/', postsController.store);
 
 // update (put)
-router.put('/:id', (req, res) => {
-    res.json('mdifico il post con id:' + req.params.id)
-});
+router.get('/:id', postsController.update);
 
 // modify (patch)
-router.patch('/:id', (req, res) => {
-    res.json('mdifica parziale del post con id:' + req.params.id)
-});
+router.get('/:id', postsController.modify);
 
 // delete
-router.delete('/:id', (req, res) => {
-    res.json('elimino il post con id:' + req.params.id)
-});
+router.get('/:id', postsController.destroy);
 
 // esporto il router
 module.exports = router;
